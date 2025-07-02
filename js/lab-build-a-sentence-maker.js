b371908f979545223e701f8fd27d29d9362fd038
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2025-07-02 14:31:06 -0300

    começando javascript

---
+++ js/lab-build-a-sentence-maker.js
@@ -0,0 +1,11 @@
+let adjective = "dumb", noun = "Pepeu", verb = "twitching", place = "street", adjective2 = "beautiful", noun2 = "Quiago Timeley"; 
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "fricking divine", noun = "Bambuli", verb = "trembling", place = "haunted house", adjective2 = "ugly", noun2 = "Quiabo Timberlake";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
---
+++ js/lab-javascript-trivia-bot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!")
+/* This lab teaches how to use mutable variables, use strings in js and the
+console.log() function - it works just like print() in Python*/
+let botName = "Pepeu", botLocation = "Volmir's Beach", favoriteLanguage = "Brazilian-Portuguese"
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = "I really know how to code in " + favoriteLanguage + ". And I don't like to wear glasses or watches when I'm coding."
+
+console.log(codingFact)
+
+codingFact = "I really like the potent slurs that " + favoriteLanguage + " has."
+
+console.log(codingFact)
+
+codingFact = "I hate " + botLocation + ", because the people in here doesn't like to talk in " + favoriteLanguage + " and that makes me really sad."
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
